f386ba8e56d2b13f78f5b598f68f3d05f3deaf1a
Atmajo <atmajoc@gmail.com>
2024-06-09 23:02:10 +0530

    v0.0.5

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,19 +2,32 @@
 
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import Cookies from "js-cookie";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+  const [menu, setMenu] = useState(false);
   return (
     <main className="flex">
-      <Sidebar />
+      <div className="hidden md:flex">
+        <Sidebar menu={menu} setMenu={setMenu} />
+      </div>
+      <div className="md:hidden absolute top-5 left-5">
+        <Image
+          src="/menu.svg"
+          alt="menu"
+          width={30}
+          height={30}
+          onClick={() => setMenu(!menu)}
+        />
+      </div>
+      <div className="md:hidden">
+        {menu && <Sidebar menu={menu} setMenu={setMenu} />}
+      </div>
       {children}
       <Toaster />
     </main>
